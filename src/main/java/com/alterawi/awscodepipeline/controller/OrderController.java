fd25c4a2b8d06b6f871171d93bff501813c38175
aymanmt <aymanmt@gmail.com>
2024-04-03 02:04:49 +1100

    Added order creation end-point

--- src/main/java/com/alterawi/awscodepipeline/controller/OrderController.java
+++ src/main/java/com/alterawi/awscodepipeline/controller/OrderController.java
@@ -4,6 +4,8 @@ import com.alterawi.awscodepipeline.dao.OrderDao;
 import com.alterawi.awscodepipeline.model.Order;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,5 +31,11 @@ public class OrderController {
     @GetMapping("/search")
     public ResponseEntity<List<Order>> getOrdersByName(@RequestParam String name) {
         return ResponseEntity.ok(orderDao.getOrdersByName(name));
+    }
+
+    @PostMapping
+    public ResponseEntity<Order> addOrder(@RequestBody Order order) {
+        orderDao.addOrder(order);
+        return ResponseEntity.ok(order);
     }
 }
--- src/main/java/com/alterawi/awscodepipeline/dao/OrderDao.java
+++ src/main/java/com/alterawi/awscodepipeline/dao/OrderDao.java
@@ -1,24 +1,34 @@
 package com.alterawi.awscodepipeline.dao;
 
 import com.alterawi.awscodepipeline.model.Order;
+import lombok.Getter;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+@Getter
 @Repository
 public class OrderDao {
-    public List<Order> getOrders() {
-        return Arrays.asList(
+
+    private final List<Order> orders;
+
+    public OrderDao() {
+        orders = new ArrayList<>(Arrays.asList(
                 new Order(1, "Book", 2, 10.0),
                 new Order(2, "Pen", 5, 5.0),
                 new Order(3, "Pencil", 10, 2.0)
-        );
+        ));
     }
 
     public List<Order> getOrdersByName(String orderName) {
         return getOrders().stream()
                 .filter(order -> order.getName().equalsIgnoreCase(orderName))
                 .toList();
+    }
+
+    public void addOrder(Order order) {
+        getOrders().add(order);
     }
 }
